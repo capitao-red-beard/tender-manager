f95af29b47e8a9e3628499a06d986c34adfbcd03
jasonvanpelt <jason.van.pelt@samskip.com>
2019-04-29 13:24:16 +0000

    fixed errors for iqs transfer

--- tender_manager.py
+++ tender_manager.py
@@ -24,7 +24,7 @@ def get_excel_column(number):
     a_b_c_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    if 25 <= number <= 702:
+    if 26 <= number <= 702:
         one = int(np.floor(number / 26) - 1)
         two = int(number % 26)
 
@@ -39,6 +39,44 @@ def get_excel_column(number):
 
     else:
         return a_b_c_list[number]
+
+def taf_to_blob_format(samskip_file=''):
+    # function to prepare the taf file for the database input
+
+    df_taf = pd.read_excel(samskip_file, sheet_name='TAF')
+    df_iqs = pd.read_excel(samskip_file, sheet_name='IQS', skiprows=1)
+
+    df_iqs.columns = [i.strip() for i in df_iqs.columns]
+
+    df_iqs = df_iqs[['Customer Lane ID', 'Pricing',
+                     'Department', 'Lane', 'No', 'Origin Country', 'Origin City',
+                     'Origin Zip Code', 'Destination Country', 'Destination City',
+                     'Destination Zip Code', 'Equipment Requested', 'Shipments / Year',
+                     'Transit Time', 'Equipment Offered', 'Payload (in ton)',
+                     'Currency', 'Total Costs', 'Margin Round 1', 'Margin % Round 1',
+                     'Round 1 Rate', 'Total Revenue round 1', 'Total current margin Round 1',
+                     'Round 1: Rank', 'Round 2: Target Rate', 'Margin Round 2',
+                     'Margin % Round 2', 'Round 2 Rate', 'Total Revenue round 2',
+                     'Total current margin Round 2', 'Round 2: Rank', 'Round 3: Target Rate',
+                     'Margin Round 3', 'Margin % Round 3', 'Round 3 Rate',
+                     'Total Revenue Round 3', 'Total current margin Round 3',
+                     'Awards Volume', 'Awards Allocation %', 'Awards Rate', 'Feedback']]
+
+    df_iqs['customer'] = df_taf.iloc[1, 2]
+    df_iqs['tender'] = df_taf.iloc[2, 2]
+    df_iqs['date'] = df_taf.iloc[3, 2]
+    df_iqs['last_tender'] = df_taf.iloc[4, 2]
+    df_iqs['account_manager'] = df_taf.iloc[5, 2]
+    df_iqs['commodity'] = df_taf.iloc[27, 5]
+    df_iqs['valid_from'] = df_taf.iloc[39, 5]
+    df_iqs['valid_to'] = df_taf.iloc[40, 5]
+    df_iqs['validity'] = df_taf.iloc[41, 5]
+    # to check later on if it is the correct row number
+    df_iqs['historic feedback'] = df_taf.iloc[77, 5]
+
+    print('Ended Successfully')
+
+    return df_iqs
 
 
 def to_tender_format(tender_file='', tender_sheet_name='', row_number=0, samskip_file='', transit_time_file='',
@@ -93,12 +131,12 @@ def to_tender_format(tender_file='', tender_sheet_name='', row_number=0, samskip
 
         sam_data = df_sam[col_sam]
 
-        if (col_sam is 'payload (in ton)') and (payload_unit is 'kg'):
+        if (col_sam == 'payload (in ton)') and (payload_unit == 'kg'):
             sam_data = [i / 1000 for i in sam_data]
 
             print('Payload changed from kg')
 
-        if col_sam is 'transit time':
+        if col_sam == 'transit time':
             transit_df = pd.read_excel(transit_time_file, sheet_name=transit_time_unit)
             transit_dic = dict(zip(list(transit_df['Samskip']), list(transit_df['Original'])))
             new_data = []
@@ -112,21 +150,28 @@ def to_tender_format(tender_file='', tender_sheet_name='', row_number=0, samskip
 
             print('transit time notation changed')
 
-        cell_letter = get_excel_column([i for i, x in enumerate(cus_headers) if x is col_cus][0])
+        cell_letter = get_excel_column([i for i, x in enumerate(cus_headers) if x == col_cus][0])
         cell_list = [cell_letter + str(num) for num in range(int(row_number) + 1, int(row_number) + len(df_sam))]
 
         for x, cell in enumerate(cell_list):
+            # print(cell)
+            # print(sht1.range(cell).value)
+            # print(list(df_sam[col_sam])[x])
+            # print(pd.isnull(list(df_sam[col_sam])[x]))
             try:
-                if (sht1.range(cell).value is list(df_sam[col_sam])[x]) or \
-                        (np.isnan(list(df_sam[col_sam])[x]) is True) or \
-                        (list(df_sam[col_sam])[x] is None) or \
-                        (list(df_sam[col_sam])[x] is 'nan'):
+                if (sht1.range(cell).value == list(df_sam[col_sam])[x]) or \
+                        (pd.isnull(list(df_sam[col_sam])[x]) == True) or \
+                        (list(df_sam[col_sam])[x] == None) or \
+                        (list(df_sam[col_sam])[x] == 'nan'):
                     continue
 
-                elif list(df_sam[col_sam])[x] is not '(Select a value)':
+                elif list(df_sam[col_sam])[x] != '(Select a value)':
+                    # print('elif',list(df_sam[col_sam])[x])
                     sht1.range(cell).value = list(df_sam[col_sam])[x]
 
             except Exception as e:
+                # print(list(df_sam[col_sam])[x])
+                # print(e)
                 pass
 
     wb.save()
@@ -134,42 +179,3 @@ def to_tender_format(tender_file='', tender_sheet_name='', row_number=0, samskip
     app.kill()
 
     print('Ended Successfully')
-
-
-def taf_to_blob_format(samskip_file=''):
-    # function to prepare the taf file for the database input
-
-    df_taf = pd.read_excel(samskip_file, sheet_name='TAF')
-    df_iqs = pd.read_excel(samskip_file, sheet_name='IQS', skiprows=1)
-
-    df_iqs.columns = [i.strip() for i in df_iqs.columns]
-
-    df_iqs = df_iqs[['Customer Lane ID', 'Pricing',
-                     'Department', 'Lane', 'No', 'Origin Country', 'Origin City',
-                     'Origin Zip Code', 'Destination Country', 'Destination City',
-                     'Destination Zip Code', 'Equipment Requested', 'Shipments / Year',
-                     'Transit Time', 'Equipment Offered', 'Payload (in ton)',
-                     'Currency', 'Total Costs', 'Margin Round 1', 'Margin % Round 1',
-                     'Round 1 Rate', 'Total Revenue round 1', 'Total current margin Round 1',
-                     'Round 1: Rank', 'Round 2: Target Rate', 'Margin Round 2',
-                     'Margin % Round 2', 'Round 2 Rate', 'Total Revenue round 2',
-                     'Total current margin Round 2', 'Round 2: Rank', 'Round 3: Target Rate',
-                     'Margin Round 3', 'Margin % Round 3', 'Round 3 Rate',
-                     'Total Revenue Round 3', 'Total current margin Round 3',
-                     'Awards Volume', 'Awards Allocation %', 'Awards Rate', 'Feedback']]
-
-    df_iqs['customer'] = df_taf.iloc[1, 2]
-    df_iqs['tender'] = df_taf.iloc[2, 2]
-    df_iqs['date'] = df_taf.iloc[3, 2]
-    df_iqs['last_tender'] = df_taf.iloc[4, 2]
-    df_iqs['account_manager'] = df_taf.iloc[5, 2]
-    df_iqs['commodity'] = df_taf.iloc[27, 5]
-    df_iqs['valid_from'] = df_taf.iloc[39, 5]
-    df_iqs['valid_to'] = df_taf.iloc[40, 5]
-    df_iqs['validity'] = df_taf.iloc[41, 5]
-    # to check later on if it is the correct row number
-    df_iqs['historic feedback'] = df_taf.iloc[77, 5]
-
-    print('Ended Successfully')
-
-    return df_iqs
