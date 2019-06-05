76c2c546b242204fe82ea5f49dc26e8f835dac7a
jasonvanpelt <jason.van.pelt@samskip.com>
2019-06-05 16:00:11 +0200

    made new main script for kryon

--- excel_handlers/iqs_manager.py
+++ excel_handlers/iqs_manager.py
@@ -2,12 +2,16 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# from azure.storage.blob import BlockBlobService
+
 import numpy as np
 import numpy.core.multiarray as multiarray
 import pandas as pd
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.tablebatch import TableBatch
+from azure.storage.blob import BlockBlobService
 
 matched_columns_location = '\\\\lebrun\\Data\\admin\\leo139\\config\\matched_columns.csv'
+transit_time_location = '\\\\lebrun\\Data\\admin\\leo139\\config\\transit_mapping.xlsx'
 # matched_columns_location = br'$MATCHED_COLUMNS_LOCATION$'
 
 # import format is file_name,sheet_name,row_number,skip_list,payload_notation,transit_time_natation
@@ -21,6 +25,7 @@ row_number = int(kryon_input[2]) - 1
 skip_list = kryon_input[3]
 payload_notation = str(kryon_input[4]).lower()
 transit_time_notation = str(kryon_input[5]).lower()
+customer = $company_name$
 
 data_columns = ['customer lane id',
                 'origin country',
@@ -43,11 +48,70 @@ data_columns = [i for i in data_columns if i not in skip_list]
 
 account_name = 'samsmdpblobdev02'
 container_name = 'raw'
-#block_blob_service = BlockBlobService(account_name='samsmdpblobdev02',
-                                      #account_key='Dr3Qut1sQMqUdTFAZ8u4fKePFfoTgMETi4/RMURiT6wcyqCFC0m1l1bnYtDDXAaFBjs4IbcXY8Xt89dRYkNY6Q==')
+block_blob_service = BlockBlobService(account_name='samsmdpblobdev02',
+                                      account_key='Dr3Qut1sQMqUdTFAZ8u4fKePFfoTgMETi4/RMURiT6wcyqCFC0m1l1bnYtDDXAaFBjs4IbcXY8Xt89dRYkNY6Q==')
+table_service = TableService(account_name='samsmdpblobdev02',
+                             account_key='Dr3Qut1sQMqUdTFAZ8u4fKePFfoTgMETi4/RMURiT6wcyqCFC0m1l1bnYtDDXAaFBjs4IbcXY8Xt89dRYkNY6Q=='))
+def create_table(table_name):
+    table_service.create_table(table_name)
 
-#def create_blob_from_path(blob_name, file_path):
-    #block_blob_service.create_blob_from_path(container_name, blob_name, file_path)
+
+def insert_entity(task, table_name='tender'):
+    table_service.insert_entity(table_name, task)
+
+
+def insert_batch_entity(df, table_name='tender'):
+    df.columns = [i.lower().replace(' ', '_') for i in df.columns]
+    df.columns = [i.replace('/', 'per') for i in df.columns]
+    df.columns = [i.replace('(', '') for i in df.columns]
+    df.columns = [i.replace(')', '') for i in df.columns]
+    df.columns = [i.replace('%', 'percent') for i in df.columns]
+    df.columns = [i.replace(':', '') for i in df.columns]
+    df.columns = [i.replace('1', 'one') for i in df.columns]
+    df.columns = [i.replace('2', 'two') for i in df.columns]
+    df.columns = [i.replace('3', 'three') for i in df.columns]
+    data = df.to_dict('records')
+
+    base = get_max_entity_row()
+
+    batch = TableBatch()
+
+    for i, d in enumerate(data, 1):
+        d['PartitionKey'] = 'iqs'
+        d['RowKey'] = str(base + i)
+
+        batch.insert_entity(d)
+
+        if i % 100 is 0 or i is len(data) + 1:
+            table_service.commit_batch(table_name, batch)
+            batch = TableBatch()
+
+
+def get_max_entity_row(partition_key='iqs', table_name='tender'):
+    tasks = table_service.query_entities(table_name, filter=f"PartitionKey eq '{partition_key}'")
+
+    current_max = 0
+
+    try:
+        for t in tasks:
+            if int(t.RowKey) > int(current_max):
+                current_max = int(t.RowKey)
+
+    except AttributeError:
+        return int(current_max)
+
+    return int(current_max) + 1
+
+
+def delete_table(table_name):
+    table_service.delete_table(table_name)
+
+
+def delete_entity(partition_key, row_key, table_name='tender'):
+    table_service.delete_entity(table_name, partition_key, row_key)
+
+def create_blob_from_path(blob_name, file_path):
+    block_blob_service.create_blob_from_path(container_name, blob_name, file_path)
 
 def read_to_dict(file, sheet, column_keys='Original', columns_items='Samskip'):
     if (file[-4:] == 'xlsx') | (file[-4:] == '.xls'):
@@ -88,17 +152,18 @@ def find_missing_columns(file, sheet, start_row=0, columns_needed=[], payload_ty
 
         if payload_type.isin(['tonnes']):
             try:
-                original['payload'] = [int(i) / 1000 for i in original['payload (in ton)']]
+                original['payload'] = [int(i) * 1000 for i in original['payload']]
             except ValueError:
-                return 'Error in pyaload conversion'
+                print('Error in payload conversion')
+                return 'Error in payload conversion'
 
-        transit_df = pd.read_excel(config_location + 'Transit_mapping.xlsx', sheet_name=transit_type)
+        transit_df = pd.read_excel(transit_time_location, sheet_name=transit_type)
         transit_dict = dict(zip(transit_df['Original'], transit_df['Samskip']))
         df['transit time'].replace(transit_dict, inplace=True)
+        df['customer'] = customer
 
-        # insert in table
-        # insert original in blob
-        # insert customer from input
+        create_blob_from_path(customer +'/'+str(file.split(r'\\')[-1]), file)
+        insert_batch_entity(df, table_name='tender')
 
         print(False)
 
