b20f55848b4f769833f573416ae1435216476a4c
jasonvanpelt <jason.van.pelt@samskip.com>
2019-06-13 09:47:45 +0200

    added origin/destination country dict and conversion

--- excel_handlers/iqs_manager.py
+++ excel_handlers/iqs_manager.py
@@ -176,6 +176,8 @@ def find_missing_columns(file, sheet, start_row=0, columns_needed=[], payload_ty
             df['transit time'].replace(transit_dict, inplace=True)
         df['customer'] = customer
 
+        df['origin country'].replace(country_dict, inplace=True)
+        df['destination country'].replace(country_dict, inplace=True)
 
         create_blob_from_path(generate_blob_name(file), file)
         insert_batch_entity(df, table_name='tender')
@@ -187,5 +189,78 @@ def find_missing_columns(file, sheet, start_row=0, columns_needed=[], payload_ty
         mc.to_csv(matched_columns_location,index=False)
 
         print(False)
+
+country_dict = {'Afghanistan': 'AF', 'Albanië': 'AL', 'Algerije': 'DZ', 'Andorra': 'AD', 'Angola': 'AO',
+                'Antigua en Barbuda': 'AG', 'Argentinië': 'AR', 'Armenië': 'AM', 'Australië': 'AU',
+                'Azerbeidzjan': 'AZ', "Bahama's": 'BS', 'Bahrein': 'BH', 'Bangladesh': 'BD', 'Barbados': 'BB',
+                'Belarus': 'BY', 'Belgie': 'BE', 'Belize': 'BZ', 'Benin': 'BJ', 'Bhutan': 'BT', 'Boeroendi': 'BI',
+                'Bolivia': 'BO', 'Bosnië-Herzegovina': 'BA', 'Botswana': 'BW', 'Brazilië': 'BR', 'Brunei': 'BN',
+                'Bulgarije': 'BG', 'Burkina Faso': 'BF', 'Cambodja': 'KH', 'Canada': 'CA',
+                'Centraal-Afrikaanse Republiek': 'CF', 'Chili': 'CL', 'China': 'CN', 'Colombia': 'CO', 'Comoren': 'KM',
+                'Congo (Brazzaville)': 'CG', 'Congo (Democratische Republiek)': 'CD', 'Costa Rica': 'CR', 'Cuba': 'CU',
+                'Cyprus': 'CY', 'Denemarken': 'DK', 'Djibouti': 'DJ', 'Dominica': 'DM', 'Dominicaanse Republiek': 'DO',
+                'Duitsland': 'DE', 'Ecuador': 'EC', 'Egypte': 'EG', 'El Salvador': 'SV', 'Equatoriaal Guinea': 'CQ',
+                'Eritrea': 'ER', 'Estland': 'EE', 'Fiji-eilanden': 'FJ', 'Filipijnen': 'PH', 'Finland': 'FI',
+                'Frankrijk': 'FR', 'Gabon': 'GA', 'Gambia': 'GM', 'Georgië': 'GE', 'Ghana': 'GH', 'Grenada': 'GD',
+                'Griekenland': 'EL', 'Guatemala': 'GT', 'Guinee': 'GN', 'Guinee Bissau': 'GW', 'Guyana': 'GY',
+                'Haïti': 'HT', 'Honduras': 'HN', 'Hongarije': 'HU', 'Ierland': 'IE', 'India': 'IN', 'Indonesië': 'ID',
+                'Irak': 'IQ', 'Iran': 'IR', 'Israël': 'IL', 'Italië': 'IT', 'Ivoorkust': 'CI', 'Jamaica': 'JM',
+                'Japan': 'JP', 'Jemen': 'YE', 'Jordanië': 'JO', 'Kaapverdië': 'CV', 'Kameroen': 'CM',
+                'Kazachstan': 'KZ', 'Kenia': 'KE', 'Kirgizië of Kirgizstan': 'KG', 'Kiribati': 'KI', 'Koeweit': 'KW',
+                'Kroatië': 'HR', 'Laos': 'LA', 'Lesotho': 'LS', 'Letland': 'LV', 'Libanon': 'LB', 'Liberia': 'LR',
+                'Libië': 'LY', 'Liechtenstein': 'LI', 'Litouwen': 'LT', 'Luxemburg': 'LU', 'Macedo': 'ië',
+                'Madagaskar': 'MG', 'Malawi': 'MW', 'Maldiven': 'MV', 'Maleisië': 'MY', 'Mali': 'ML', 'Malta': 'MT',
+                'Marokko': 'MA', 'Marshalleilanden': 'MH', 'Mauritanië': 'MR', 'Mauritius': 'MU', 'Mexico': 'MX',
+                'Micronesia': 'FM', 'Moldavië': 'MD', 'Monaco ': 'MC', 'Mongolië': 'MN', 'Mozambique': 'MZ',
+                'Myanmar': 'MM', 'Namibië': 'NA', 'Nauru': 'NR', 'Nederland': 'NL', 'Nepal': 'NP', 'Nicaragua': 'NI',
+                'Nieuw-Zeeland': 'NZ', 'Niger': 'NE', 'Nigeria': 'NG', 'Noord-Korea': 'KP', 'Noorwegen': 'NO',
+                'Oeganda': 'UG', 'Oekraïne': 'UA', 'Oezbekistan': 'UZ', 'Oman': 'OM', 'Oost-Timor': 'TL',
+                'Oostenrijk ': 'AT', 'Pakistan': 'PK', 'Palau': 'PW', 'Panama': 'PA', 'Papoea-Nieuw-Guinea': 'PG',
+                'Paraguay': 'PY', 'Peru': 'PE', 'Polen': 'PL', 'Portugal': 'PT', 'Qatar': 'QA', 'Roemenië': 'RO',
+                'Rusland': 'RU', 'Rwanda': 'RW', 'Saint Kitts en Nevis': 'KN', 'Saint Lucia': 'LC',
+                'Saint Vincent en de Grenadines': 'VC', 'Salomonseilanden': 'SB', 'Samoa': 'WS', 'San Marino': 'SM',
+                'Sao Tomé en Principe': 'ST', 'Saoedi-Arabië': 'SA', 'Senegal': 'SN', 'Servië en Montenegro': 'YU',
+                'Seychellen': 'SC', 'Sierra Leone': 'SL', 'Singapore ': 'SG', 'Slovenië': 'SI', 'Slowakije': 'SK',
+                'Soedan': 'SD', 'Somalië': 'SO', 'Spanje': 'ES', 'Sri Lanka': 'LK', 'Suriname': 'SR', 'Swaziland': 'SZ',
+                'Syrië': 'SY', 'Tadzjikistan': 'TJ', 'Taiwan': 'TW', 'Tanzania': 'TZ', 'Thailand': 'TH', 'Togo': 'TG',
+                'Tonga': 'TO', 'trinidad en Tobago': 'TT', 'Tsjaad': 'TD', 'Tsjechië': 'CZ', 'Tunesië': 'TN',
+                'Turkmenistan': 'TM', 'Turkije': 'tr', 'Tuvalu': 'TV', 'United Kingdom': 'GB', 'Uruguay': 'UY',
+                'Vanuatu': 'VU', 'Vaticaanstad': 'VA', 'Venezuela': 'VE', 'Verenigde Arabische Emiraten': 'AE',
+                'Verenigde Staten': 'US', 'Vietnam': 'VN', 'IJsland': 'IS', 'Zambia': 'ZM', 'Zimbabwe': 'ZW',
+                'Zuid-Afrika': 'ZA', 'Zuid-Korea': 'KR', 'Zweden': 'SE', 'Zwitserland': 'CH', 'Albania': 'AL',
+                'Algeria': 'DZ', 'American Samoa': 'AS', 'Anguilla': 'AI', 'Antarctica': 'AQ',
+                'Antigua and Barbuda': 'AG', 'Argentina': 'AR', 'Armenia': 'AM', 'Aruba': 'AW', 'Australia': 'AU',
+                'Austria': 'AT', 'Azerbaijan': 'AZ', 'Bahamas': 'BS', 'Bahrain': 'BH', 'Belgium': 'BE', 'Bermuda': 'BM',
+                'Bonaire': 'BQ', 'Bosnia and Herzegovina': 'BA', 'Brazil': 'BR', 'British Indian Ocean Territory': 'IO',
+                'British Virgin Islands': 'VG', 'Bulgaria': 'BG', 'Burundi': 'BI', 'Cambodia': 'KH', 'Cameroon': 'CM',
+                'Cape Verde': 'CV', 'Cayman Islands': 'KY', 'Central African Republic': 'CF', 'Chad': 'TD',
+                'Chile': 'CL', 'Christmas Island': 'CX', 'Cocos Islands': 'CC', 'Comoros': 'KM', 'Congo': 'CG',
+                'Cook Islands': 'CK', 'Croatia': 'HR', 'Curacao': 'CW', 'Czech Republic': 'CZ',
+                'Democratic Republic of the Congo': 'CD', 'Denmark': 'DK', 'Dominican Republic': 'DO',
+                'East Timor': 'TL', 'Egypt': 'EG', 'Equatorial Guinea': 'GQ', 'Estonia': 'EE', 'Ethiopia': 'ET',
+                'Falkland Islands': 'FK', 'Faroe Islands': 'FO', 'Fiji': 'FJ', 'France': 'FR', 'French Guinea': 'GF',
+                'French Polynesia': 'PF', 'Georgia': 'GE', 'Germany': 'DE', 'Gibraltar': 'GI', 'Greece': 'GR',
+                'Greenland': 'GL', 'Guadeloupe': 'GP', 'Guam': 'GU', 'Guernsey': 'GG', 'Guinea': 'GN',
+                'Guinea-Bissau': 'GW', 'Haiti': 'HT', 'Hong Kong': 'HK', 'Hungary': 'HU', 'Iceland': 'IS',
+                'Indonesia': 'ID', 'Iraq': 'IQ', 'Ireland': 'IE', 'Isle of Man': 'IM', 'Israel': 'IL', 'Italy': 'IT',
+                'Ivory Coast': 'CI', 'Jersey': 'JE', 'Jordan': 'JO', 'Kazakhstan': 'KZ', 'Kenya': 'KE', 'Kosovo': 'KS',
+                'Kuwait': 'KW', 'Kyrgyzstan': 'KG', 'Latvia': 'LV', 'Lebanon': 'LB', 'Libya': 'LY', 'Lithuania': 'LT',
+                'Luxembourg': 'LU', 'Macao': 'MO', 'Macedonia': 'MK', 'Madagascar': 'MG', 'Malaysia': 'MY',
+                'Maldives': 'MV', 'Marshall Islands': 'MH', 'Martinique': 'MQ', 'Mauritania': 'MR', 'Mayotte': 'YT',
+                'Moldova': 'MD', 'Monaco': 'MC', 'Mongolia': 'MN', 'Montenegro': 'ME', 'Montserrat': 'MS',
+                'Morocco': 'MA', 'Netherlands': 'NL', 'Netherlands Antilles': 'AN', 'New Caledonia': 'NC',
+                'New Zealand': 'NZ', 'Niue': 'NU', 'North Korea': 'KP', 'Northern Ireland': 'NI',
+                'Northern Mariana Islands': 'MP', 'Norway': 'NO', 'Papua New Guinea': 'PG', 'Philippines': 'PH',
+                'Pitcairn': 'PN', 'Poland': 'PL', 'Puerto Rico': 'PR', 'Republic of the Congo': 'CG', 'Reunion': 'RE',
+                'Romania': 'RO', 'Russia': 'RU', 'Saint Barthelemy': 'BL', 'Saint Helena': 'SH',
+                'Saint Kitts and Nevis': 'KN', 'Saint Martin': 'MF', 'Saint Pierre and Miquelon': 'PM',
+                'Saint Vincent and the Grenadines': 'VC', 'Sao Tome and Principe': 'ST', 'Saudi Arabia': 'SA',
+                'Serbia': 'RS', 'Seychelles': 'SC', 'Singapore': 'SG', 'Sint Maarten': 'SX', 'Slovakia': 'SK',
+                'Slovenia': 'SI', 'Solomon Islands': 'SB', 'Somalia': 'SO', 'South Africa': 'ZA', 'South Korea': 'KR',
+                'Spain': 'ES', 'Sudan': 'SD', 'Svalbard and Jan Mayen': 'SJ', 'Sweden': 'SE', 'Switzerland': 'CH',
+                'Syria': 'SY', 'Tajikistan': 'TJ', 'Tokelau': 'TK', 'Trinidad and Tobago': 'TT', 'Tunisia': 'TN',
+                'Turkey': 'TR', 'Turks and Caicos Islands': 'TC', 'U.S. Virgin Islands': 'VI', 'Uganda': 'UG',
+                'Ukraine': 'UA', 'United Arab Emirates': 'AE', 'United States': 'US', 'Uzbekistan': 'UZ',
+                'Vatican': 'VA', 'Wallis and Futuna': 'WF', 'Western Sahara': 'EH', 'Yemen': 'YE'}
 
 find_missing_columns(file_name,sheet_name,row_number,data_columns,payload_notation,transit_time_notation)
